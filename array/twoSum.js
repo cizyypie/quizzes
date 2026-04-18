b4d294b8472391b4a4cb44d2f0b36abe43416a86
Joan <joanitatimbinp@gmail.com>
2026-04-18 12:00:00 +0700

    created two sum quiz

---
+++ array/twoSum.js
@@ -0,0 +1,18 @@
+/*
+l. make a map
+2. 
+ */
+
+function twoSum(nums, target){
+    const map = new Map();
+
+    for(let i=0; i<nums.length; i++){
+        const comp = target-nums[i]
+
+        if(map.has(comp)){
+            return[map.get(comp),i]
+        }
+        map.set(nums[i],j)
+    }
+    return[]
+}
