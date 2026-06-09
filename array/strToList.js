8cb85798f9f566d096b95bd92c504ebb3a19a3c9
Joan <joanitatimbinp@gmail.com>
2026-06-09 22:57:10 +0700

    create strToList

---
+++ array/strToList.js
@@ -0,0 +1,20 @@
+function strToList(str) {
+  return str.split('').filter(char => {
+    if (char === ' ') {
+      return false;
+    }
+
+    if (char >= '0' && char <= '9') {
+      if (parseInt(char) > 5) {
+        return false;
+      }
+    }
+    
+    return true;
+  });
+}
+
+// Examples
+console.log(strToList("my string"));  // ["m", "y", "s", "t", "r", "i", "n", "g"]
+console.log(strToList("2 dollar"));   // ["5", "d", "o", "l", "l", "a", "r"]
+console.log(strToList("6 cents"));    // ["c", "e", "n", "t", "s"]
